461687fcf985cfaafed178b66b4145e908332920
Nilton Santos <nilton.naab@gmail.com>
2025-04-26 21:15:39 -0400

    chore: remove outdated code
    Revert "chore: remove outdated code"
    
    This reverts commit f31ca6b89886f5464280fd92c3b0d45036f0366e.
    
    chore: remove outdated code
    
    chore: remove outdated code
    
    chore: change logos

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,8 @@ import {
 } from "react-router-dom";
 import "./styles/app-style.css";
 import Navbar from "./components/Navbar";
-// import Projects from "./pages/Project";
-// import Resume from "./pages/Resume";
 import About from "./pages/Home";
-import Blog from "./pages/Blog";
-// import Post from "./pages/Post";
+
 
 const App: FC = () => {
   return (
@@ -21,10 +18,6 @@ const App: FC = () => {
         <main className="content">
           <Routes>
             <Route path="/" element={<Navigate to="/home" />} />
-            {/* <Route path="/projects" element={<Projects />} /> */}
-            {/* <Route path="/resume" element={<Resume />} /> */}
-            <Route path="/blog" element={<Blog />} />
-            {/* <Route path="/blog/:slug" element={<Post />} /> */}
             <Route path="/home" element={<About />} />
           </Routes>
         </main>
